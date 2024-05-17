37dd98c1fcdddff0d8abdd2154f8b978262b7504
EdiedRamos <edieisa22@gmail.com>
2024-05-16 21:21:21 -0500

    chore: add colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cc-black": "#1B1D1F",
+        "cc-dark": "#282B30",
+        "cc-blue": "#4E80EE",
+        "cc-gray": "#6C727F",
+        "cc-light": "#D2D5DA",
+      },
+      fontSize: {
+        "cs-50": "12px",
+        "cs-100": "14px",
+        "cs-200": "16px",
+        "cs-300": "32px",
+      },
+    },
+  },
+  corePlugins: {
+    fontFamily: false,
   },
   plugins: [],
 };
