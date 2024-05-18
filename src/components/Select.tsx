443461604ba24b3e549fb359e0b5044d5ccce810
EdiedRamos <edieisa22@gmail.com>
2024-05-17 23:31:32 -0500

    style: udpate select options top margin

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -53,7 +53,7 @@ export const Select = () => {
         <ExpandDown className={showOptions ? "rotate-180" : ""} />
       </div>
       {showOptions && (
-        <div className="absolute bg-cc-black w-[200px] rounded-lg mt-1 text-cc-light border-[3px] border-cc-dark">
+        <div className="absolute bg-cc-black w-[200px] rounded-lg mt-[1px] text-cc-light border-[3px] border-cc-dark">
           {options.map(({ id, name }) => (
             <div
               className="p-2 border-t-[3px] border-t-cc-dark first:border-none hover:bg-cc-blue overflow-hidden hover:cursor-pointer first:rounded-t-lg last:rounded-b-lg"
