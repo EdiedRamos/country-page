94365d560af5e6b9408eb4e52583df16615fcbe1
EdiedRamos <edieisa22@gmail.com>
2024-05-18 00:49:05 -0500

    chore: add regions to data

--- src/components/Region.tsx
+++ src/components/Region.tsx
@@ -1,18 +1,11 @@
-const MOCK = [
-  { id: "1", value: "Americas", isSelected: true },
-  { id: "2", value: "Antarctic", isSelected: false },
-  { id: "3", value: "Africa", isSelected: true },
-  { id: "4", value: "Asia", isSelected: true },
-  { id: "5", value: "Europe", isSelected: true },
-  { id: "6", value: "Oceania", isSelected: false },
-];
+import { REGIONS } from "@/utils";
 
 export const Region = () => {
   return (
     <div className="w-[300px]">
       <p className="text-cc-gray text-cs-50 font-medium">Region</p>
       <div className="flex flex-wrap gap-3">
-        {MOCK.map((opt) => (
+        {REGIONS.map((opt) => (
           <button
             className={`p-3 text-cc-gray hover:bg-cc-dark rounded-xl ${
               opt.isSelected ? "bg-cc-dark text-cc-light" : ""
--- src/utils/data/index.ts
+++ src/utils/data/index.ts
@@ -3,3 +3,12 @@ export const SELECT_OPTIONS = [
   { id: "2", name: "Name" },
   { id: "3", name: "Population", default: true },
 ];
+
+export const REGIONS = [
+  { id: "1", value: "Americas", isSelected: true },
+  { id: "2", value: "Antarctic", isSelected: false },
+  { id: "3", value: "Africa", isSelected: true },
+  { id: "4", value: "Asia", isSelected: true },
+  { id: "5", value: "Europe", isSelected: true },
+  { id: "6", value: "Oceania", isSelected: false },
+];
