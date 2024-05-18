1705de96bb442308777ddafe948fc279ff90fd56
EdiedRamos <edieisa22@gmail.com>
2024-05-18 00:32:41 -0500

    chore: add data and update content layer

--- src/components/ContentLayer.tsx
+++ src/components/ContentLayer.tsx
@@ -1,17 +1,14 @@
 import { Content, Select } from "@/components";
 
+import { SELECT_OPTIONS } from "@/utils";
+
 export const ContentLayer = () => {
   return (
     <div className="bg-cc-black">
       <Content />
       <div>
-        <Select
-          options={[
-            { id: "1", name: "Area" },
-            { id: "2", name: "Name", default: true },
-            { id: "3", name: "Population" },
-          ]}
-        />
+        <p className="text-cc-gray text-cs-50 font-medium">Sort by</p>
+        <Select options={SELECT_OPTIONS} />
       </div>
     </div>
   );
---
+++ src/utils/data/index.ts
@@ -0,0 +1,5 @@
+export const SELECT_OPTIONS = [
+  { id: "1", name: "Area" },
+  { id: "2", name: "Name" },
+  { id: "3", name: "Population", default: true },
+];
