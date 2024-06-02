2fcec8d2537beab4cee657a50f016dc690e75e0b
EdiedRamos <edieisa22@gmail.com>
2024-06-01 22:28:38 -0500

    style: update table spacing

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -3,7 +3,7 @@ import { TableHeader } from "./TableHeader";
 
 export const Table = () => {
   return (
-    <table className="w-full">
+    <table className="w-full border-separate border-spacing-y-6">
       <TableHeader />
       <TableBody />
     </table>
