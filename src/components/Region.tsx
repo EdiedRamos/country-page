a594bf8000df3a074cc3b8c141e16deba7259206
EdiedRamos <edieisa22@gmail.com>
2024-06-21 04:57:22 -0500

    feat: add click handle for regions

--- src/components/Region.tsx
+++ src/components/Region.tsx
@@ -1,12 +1,26 @@
 import { REGIONS } from "@/utils";
+import { useState } from "react";
 
 export const Region = () => {
+  const [regions, setRegions] = useState<typeof REGIONS>(REGIONS);
+
+  const handleSelect = (id: string): void => {
+    setRegions((prev) =>
+      prev.map((region) =>
+        region.id === id
+          ? { ...region, isSelected: !region.isSelected }
+          : region
+      )
+    );
+  };
+
   return (
     <div className="">
       <p className="text-cc-gray text-cs-50 font-medium mb-2">Region</p>
       <div className="flex flex-wrap gap-3">
-        {REGIONS.map((opt) => (
+        {regions.map((opt) => (
           <button
+            onClick={() => handleSelect(opt.id)}
             className={`p-3 text-cc-gray hover:bg-cc-dark rounded-xl text-cs-100 ${
               opt.isSelected ? "bg-cc-dark text-cc-light" : ""
             }`}
