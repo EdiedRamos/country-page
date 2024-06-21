d466b4dcb780f3c37388546335479f889f7d9a06
EdiedRamos <edieisa22@gmail.com>
2024-06-21 05:03:43 -0500

    feat: implement debounce for searching

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,22 @@
+import { ChangeEvent, useEffect, useState } from "react";
+
 import { SearchIcon } from "@/assets";
 
 export const Search = () => {
+  const [searchTarget, setSearchTarget] = useState<string>("");
+
+  const handleSearchTargetChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchTarget(event.target.value);
+  };
+
+  useEffect(() => {
+    const debounceId = setTimeout(() => {
+      console.log(`Searching: ${searchTarget}`);
+    }, 500);
+
+    return () => clearTimeout(debounceId);
+  }, [searchTarget]);
+
   return (
     <div className="relative">
       <img
@@ -11,6 +27,8 @@ export const Search = () => {
         className="w-full sm:w-[320px] text-cs-100 text-cc-gray placeholder:text-cc-gray p-3 pl-10 rounded-xl bg-cc-dark outline-none focus:ring focus:ring-cc-blue"
         type="text"
         placeholder="Search by Name, Region, Subregion"
+        value={searchTarget}
+        onChange={handleSearchTargetChange}
       />
     </div>
   );
