d87905177d72e22b437f7346add02b1e887cf1cd
EdiedRamos <edieisa22@gmail.com>
2024-06-25 05:18:17 -0500

    chore: add get preview and searching country api

---
+++ src/services/api/countryAPI.ts
@@ -0,0 +1,18 @@
+import type { PreviewAndSearchingResponse } from "@/types";
+import axios from "axios";
+
+class CountryAPI {
+  public async getPreviewAndSearcingData(): Promise<PreviewAndSearchingResponse | null> {
+    try {
+      // TODO: Create a base axios
+      const response = await axios.get<PreviewAndSearchingResponse>(
+        "https://restcountries.com/v3.1/all?fields=name,flags,independent,unMember,region,subregion,area,population"
+      );
+      return response.data;
+    } catch {
+      return null;
+    }
+  }
+}
+
+export const countryAPI = new CountryAPI();
