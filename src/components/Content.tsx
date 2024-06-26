5f2c67fd3a00a089e67baf67b7fa0d8e213c1026
EdiedRamos <edieisa22@gmail.com>
2024-06-26 02:37:16 -0500

    feat: show the number of countries

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,11 +1,15 @@
 import { Search } from "@/components";
+import { useAppSelector } from "@/store";
 
 export const Content = () => {
+  const numberOfCountries = useAppSelector(
+    (store) => store.countryReducer.previewFiltered?.length ?? 0
+  );
   return (
     <div>
       <div className="flex flex-col md:flex-row gap-3 flex-wrap  items-center justify-center md:justify-between">
         <p className="text-cc-gray font-medium text-cs-200">
-          Found 234 countries
+          Found {numberOfCountries} countries
         </p>
         <Search />
       </div>
