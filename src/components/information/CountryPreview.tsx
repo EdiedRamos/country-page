8f5b7bee5d6aa49e120db14922f51940901f623e
EdiedRamos <edieisa22@gmail.com>
2024-06-28 03:56:45 -0500

    feat: add scroll to top when a new country information is loaded

--- src/components/information/CountryPreview.tsx
+++ src/components/information/CountryPreview.tsx
@@ -7,9 +7,14 @@ interface Props extends CountryPreviewDTO {}
 export const CountryPreview = ({ name, flag }: Props) => {
   const navigate = useNavigate();
 
+  const handleClick = () => {
+    navigate(URLS.information.replace(":country", name));
+    scrollTo(0, 0);
+  };
+
   return (
     <div
-      onClick={() => navigate(URLS.information.replace(":country", name))}
+      onClick={handleClick}
       className="w-20 flex flex-col items-center gap-2 hover:opacity-55 hover:cursor-pointer hover:transition-all"
     >
       <img className="w-20 rounded-md" src={flag} alt={`${name} falg`} />
