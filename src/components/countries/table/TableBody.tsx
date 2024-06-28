550c993a346147e4068d568967d2ffecc5e2ab0c
EdiedRamos <edieisa22@gmail.com>
2024-06-27 22:18:33 -0500

    feat: add table redirect to country information

--- src/components/countries/table/TableBody.tsx
+++ src/components/countries/table/TableBody.tsx
@@ -1,27 +1,37 @@
 import { CountryPreviewAndSearchingDTO } from "@/dtos";
+import { URLS } from "@/utils";
+import { useNavigate } from "react-router";
 
 interface Props {
   countries: CountryPreviewAndSearchingDTO[];
 }
 
 export const TableBody = ({ countries }: Props) => {
+  const navigate = useNavigate();
+
   return (
     <tbody>
-      {countries.map((info) => (
-        <tr key={info.name} className="text-cc-light">
+      {countries.map((country) => (
+        <tr
+          key={country.name}
+          onClick={() =>
+            navigate(URLS.information.replace(":country", country.name))
+          }
+          className="text-cc-light border-2 hover:bg-cc-dark hover:cursor-pointer"
+        >
           <td className="w-[12%]">
             <img
               className="w-[50px] h-[38px] object-cover rounded-md"
-              src={info.flag}
-              alt={`${info.name} flag`}
+              src={country.flag}
+              alt={`${country.name} flag`}
             />
           </td>
-          <td className="w-[22%]">{info.name}</td>
-          <td className="w-[22%]">{info.population.toLocaleString("en")}</td>
+          <td className="w-[22%]">{country.name}</td>
+          <td className="w-[22%]">{country.population.toLocaleString("en")}</td>
           <td className="hidden sm:table-cell w-[22%]">
-            {info.area.toLocaleString("en")}
+            {country.area.toLocaleString("en")}
           </td>
-          <td className="hidden xl:table-cell w-[22%]">{info.region}</td>
+          <td className="hidden xl:table-cell w-[22%]">{country.region}</td>
         </tr>
       ))}
     </tbody>
