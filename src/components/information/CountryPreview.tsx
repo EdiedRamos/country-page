29cc80cc0c89c8d35690058bc9afd1bddef10626
EdiedRamos <edieisa22@gmail.com>
2024-06-28 03:51:09 -0500

    feat: render country borders

--- src/components/information/CountryPreview.tsx
+++ src/components/information/CountryPreview.tsx
@@ -1,12 +1,12 @@
-export const CountryPreview = () => {
+import { CountryPreviewDTO } from "@/dtos/country/countryPreviewDTO";
+
+interface Props extends CountryPreviewDTO {}
+
+export const CountryPreview = ({ name, flag }: Props) => {
   return (
     <div className="w-20 flex flex-col items-center gap-2">
-      <img
-        className="w-20 rounded-md"
-        src="https://flagcdn.com/co.svg"
-        alt="flag"
-      />
-      <p className="text-cs-50 text-cc-light">Afghanistan</p>
+      <img className="w-20 rounded-md" src={flag} alt={`${name} falg`} />
+      <p className="text-cs-50 text-cc-light">{name}</p>
     </div>
   );
 };
--- src/components/information/NeighbouringCountries.tsx
+++ src/components/information/NeighbouringCountries.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
+
 import { CountryPreview } from "./CountryPreview";
+import { CountryPreviewDTO } from "@/dtos/country/countryPreviewDTO";
+import { countryRepository } from "@/repositories/countryRepository";
 
 interface Props {
   borders: string[];
 }
 
 export const NeighbouringCountries = ({ borders }: Props) => {
-  if (borders.length === 0) return <></>;
+  const [borderCountries, setBorderCountries] = useState<CountryPreviewDTO[]>(
+    []
+  );
+
+  useEffect(() => {
+    countryRepository
+      .getBorderCountries(borders.join(","))
+      .then((data) => setBorderCountries(data ?? []));
+  }, [borders]);
+
+  if (borderCountries.length === 0) return <></>;
 
   return (
     <>
@@ -13,10 +27,9 @@ export const NeighbouringCountries = ({ borders }: Props) => {
         Neighbouring Countries
       </p>
       <div className="mt-4 mb-10 px-5 flex flex-wrap gap-4">
-        <CountryPreview />
-        <CountryPreview />
-        <CountryPreview />
-        <CountryPreview />
+        {borderCountries.map((country) => (
+          <CountryPreview key={country.name} {...country} />
+        ))}
       </div>
     </>
   );
