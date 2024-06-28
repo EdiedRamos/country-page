33cd48174be7d306089c6b783d319f6e584512f2
EdiedRamos <edieisa22@gmail.com>
2024-06-28 03:54:30 -0500

    feat: implement border navigation

--- src/components/information/CountryPreview.tsx
+++ src/components/information/CountryPreview.tsx
@@ -1,10 +1,17 @@
 import { CountryPreviewDTO } from "@/dtos/country/countryPreviewDTO";
+import { URLS } from "@/utils";
+import { useNavigate } from "react-router";
 
 interface Props extends CountryPreviewDTO {}
 
 export const CountryPreview = ({ name, flag }: Props) => {
+  const navigate = useNavigate();
+
   return (
-    <div className="w-20 flex flex-col items-center gap-2">
+    <div
+      onClick={() => navigate(URLS.information.replace(":country", name))}
+      className="w-20 flex flex-col items-center gap-2 hover:opacity-55 hover:cursor-pointer hover:transition-all"
+    >
       <img className="w-20 rounded-md" src={flag} alt={`${name} falg`} />
       <p className="text-cs-50 text-cc-light">{name}</p>
     </div>
