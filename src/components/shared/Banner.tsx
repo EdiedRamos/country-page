500bb1242a0a47e30b39e1ed4588a749fcd8e15e
EdiedRamos <edieisa22@gmail.com>
2024-06-27 22:20:55 -0500

    style: update banner repeat and bg

--- src/components/shared/Banner.tsx
+++ src/components/shared/Banner.tsx
@@ -3,7 +3,7 @@ import { HeroImage, Logo } from "@/assets";
 export const Banner = () => {
   return (
     <div
-      className="h-[300px] overflow-hidden bg-center flex justify-center items-center"
+      className="h-[300px] overflow-hidden bg-center flex justify-center items-center bg-no-repeat bg-black"
       style={{ backgroundImage: `url(${HeroImage})` }}
     >
       <Logo />
