7eea5eed14cf76e55716c7e8ae0bd8d86d61a6b1
EdiedRamos <edieisa22@gmail.com>
2024-06-28 03:44:46 -0500

    chore: add country preview repository

--- src/repositories/countryRepository.ts
+++ src/repositories/countryRepository.ts
@@ -1,7 +1,9 @@
 import { CountryInformationDTO, CountryPreviewAndSearchingDTO } from "@/dtos";
 
+import { CountryPreviewDTO } from "@/dtos/country/countryPreviewDTO";
 import { countryAPI } from "@/services/api/countryAPI";
 import { countryInformationMapper } from "@/services/mappers/country/countryInformationMapper";
+import { countryPreviewMapper } from "@/services/mappers/country/countryPreviewMapper";
 import { previewAndSearchingMapper } from "@/services/mappers";
 
 class CountryRepository {
@@ -22,6 +24,14 @@ class CountryRepository {
     const countryInformation = await countryAPI.fetchCountryByName(countryName);
     if (!countryInformation) return null;
     return countryInformationMapper(countryInformation[0]);
+  }
+
+  public async getBorderCountries(
+    borders: string
+  ): Promise<CountryPreviewDTO[] | null> {
+    const borderCountries = await countryAPI.fetchCountriesByBorders(borders);
+    if (!borderCountries) return null;
+    return borderCountries.map(countryPreviewMapper);
   }
 }
 
