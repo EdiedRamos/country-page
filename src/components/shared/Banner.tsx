7bb95f82ff23d72b42a438937551db1810f37bb4
EdiedRamos <edieisa22@gmail.com>
2024-06-29 20:13:35 -0500

    feat: add redirect to /countries in the banner logo

--- src/components/shared/Banner.tsx
+++ src/components/shared/Banner.tsx
@@ -1,12 +1,19 @@
 import { HeroImage, Logo } from "@/assets";
 
+import { URLS } from "@/utils";
+import { useNavigate } from "react-router";
+
 export const Banner = () => {
+  const navigate = useNavigate();
+
   return (
     <div
       className="h-[300px] overflow-hidden bg-center flex justify-center items-center bg-no-repeat bg-black"
       style={{ backgroundImage: `url(${HeroImage})` }}
     >
-      <Logo />
+      <button onClick={() => navigate(URLS.countries)}>
+        <Logo />
+      </button>
     </div>
   );
 };
